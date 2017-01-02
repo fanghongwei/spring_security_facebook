2132affb5b57c4f787a7b751f30bc77e4adff1a2
fanghongwei <fhw8234@gmail.com>
2017-01-02 23:02:04 +0800

    added one line that can get the user information from facebook

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,17 @@ public class SampleController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Fang & Jingjing, - Happy New Year !!! ";
+
+        OAuth2Authentication oAuth2Authentication =
+                (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+
+        Authentication userAuthentication = oAuth2Authentication.getUserAuthentication();
+
+        System.out.println("######################");
+        System.out.println(userAuthentication.getDetails());
+        System.out.println("######################");
+
+        return "Hello Fang & Jingjing, - Happy New Year !!!  (" + userAuthentication.getName() + ")";
     }
 
 }
